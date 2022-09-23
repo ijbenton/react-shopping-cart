a0772bb1355b65b1846c83efcdbe4b132ba901ad
Ian Benton <bentonian1@gmail.com>
2022-09-22 20:52:20 -0700

    routing setup and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default App;
